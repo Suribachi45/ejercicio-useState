495fdfde54197654db7b1ab59b4918b772168e2a
Suribachi45 <adrinpajuelo@gmail.com>
2025-03-03 22:17:08 +0100

    subida

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,31 @@ import './App.css'
 
 function App() {
  //Aquí tu código
-}
+ const [nombre, setNombre] = useState("Sofía");
+  const [nuevoNombre, setNuevoNombre] = useState('');
+
+  const manejarClic = () => {
+    if (nuevoNombre.trim() !== '') {
+      setNombre(nuevoNombre);
+      setNuevoNombre('');
+    }
+  };
+
+  return (
+    <div class="formulario">
+      <h2>Nombre del Profesor: {nombre}</h2>
+      <div>
+        <input
+          type="text"
+          value={nuevoNombre}
+          onChange={(e) => setNuevoNombre(e.target.value)}
+          placeholder="Ingresa un nombre"
+        />
+        <button onClick={manejarClic}>Actualizar</button>
+      </div>
+    </div>
+  );
+};
+
 
 export default App
